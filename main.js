46ccebd4620e745b06a36044eb993a8bf13fbe40
DANILA <greppidanila@gmail.com>
2023-03-18 21:00:14 -0300

    Css e inicio de Js

--- main.js
+++ main.js
@@ -1,20 +1,55 @@
-let input = document.querySelector(".input");
-let buttons = document.querySelectorAll(".button");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (button.classList.contains("number")) {
-      input.value += button.textContent;
-    } else if (button.classList.contains("operator")) {
-      input.value += ` ${button.textContent} `;
-    } else if (button.classList.contains("decimal")) {
-      if (!input.value.includes(".")) {
-        input.value += button.textContent;
-      }
-    } else if (button.classList.contains("clear")) {
-      input.value = "";
-    } else if (button.classList.contains("equals")) {
-      input.value = eval(input.value);
+class Calculator {
+  constructor(previousTextElement, currentTextElement) {
+    this.previousTextElement = previousTextElement;
+    this.currentTextElement = currentTextElement;
+    this.clear();
+  }
+  clear() {
+    this.current = "";
+    this.previous = "";
+    this.operation = undefined;
+  }
+  delete() {
+    this.current = this.current.toString().slice(0, -1);
+  }
+  appendNumber(number) {
+    if (number === "." && this.current.includes(".")) return;
+    this.current = this.current.toString() + number.toString();
+  }
+  chooseOperation(operation) {
+    if (this.current == "") return;
+    if (this.previous !== "") {
+      this.compute();
     }
-  });
-});
+    this.operation = operation;
+    this.previous = this.current;
+    this.current = "";
+  }
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previous);
+    const current = parseFloat(this.current);
+    if (isNaN(prev) || isNaN(current)) return;
+
+    switch (this.operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "*":
+        computation = prev * current;
+        break;
+      case "+":
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+
+    this.current = computation.toString();
+    this.operation = undefined;
+    this.previous = "";
+  }
+}
